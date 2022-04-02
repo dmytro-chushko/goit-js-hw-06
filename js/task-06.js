aaca73685b06e36c1188d7e2c36e652b08ad8e1b
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-04-02 16:24:18 +0300

    fixed 6th task again

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,10 @@
 document.querySelector('#validation-input').addEventListener('blur', function () {
-  Number(this.dataset.length) === this.value.length ? this.classList.add('valid') : this.classList.add('invalid');
+  if (Number(this.dataset.length) === this.value.length) {
+    this.classList.add('valid');
+    this.classList.remove('invalid');
+  } else {
+    this.classList.add('invalid');
+  };
 });
+
+    
