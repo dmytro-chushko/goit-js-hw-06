32c5b76b646be2b6944a31821f8f2830efdc7de7
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-04-02 15:57:06 +0300

    fixed 6th task

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,3 @@
 document.querySelector('#validation-input').addEventListener('blur', function () {
-  Number(this.dataset.length) >= this.value.length ? this.classList.add('valid') : this.classList.add('invalid');
+  Number(this.dataset.length) === this.value.length ? this.classList.add('valid') : this.classList.add('invalid');
 });
